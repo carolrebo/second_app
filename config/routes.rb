f3307dda6276f22574f0bb9db96f71c98925006e
carol <carolrebolledoc@gmail.com>
2018-06-01 21:00:42 -0400

    cuarto commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'page/index'
+  get 'students/index'
+  root 'students#index'
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
